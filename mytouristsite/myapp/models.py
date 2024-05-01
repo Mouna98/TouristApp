e484369d5bf29e21815678c4706e83f7be9a1f22
mou <mouna.erraji@stud.unifi.it>
2024-05-01 19:45:00 +0200

    Prove inserimento Quiz

--- mytouristsite/myapp/models.py
+++ mytouristsite/myapp/models.py
@@ -5,9 +5,16 @@ from django.core.validators import MinValueValidator
 class Visit(models.Model):
     name = models.CharField(max_length=100)
     place = models.CharField(max_length=100)
-    duration = models.IntegerField(default=1, validators=[MinValueValidator(1)])  # assuming duration is in days
+    duration = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
 
+class Domanda(models.Model):
+    testo = models.CharField(max_length=255)
+    risposta = models.BooleanField()
+    descrizione = models.TextField()
 
+    def __str__(self):
+        return self.testo
--- mytouristsite/myapp/views.py
+++ mytouristsite/myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .forms import VisitForm
 import json
-from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.http import HttpResponseNotFound, HttpResponseServerError, HttpResponse, HttpResponseRedirect, JsonResponse
+from .models import Visit
 
 
 def home(request):
@@ -26,18 +29,15 @@ def mostra_itinerario(request, name, place, duration):
         with open('myapp/itinerari.json') as file:
             itinerari_data = json.load(file)
 
-           # print(itinerari_data)
-
         itinerario_citta = itinerari_data.get(place.lower(), {})
         giorni_itinerario = {}
+        domande_vero_falso = []
+
 
         # Ottieni l'itinerario per la città e il numero di giorni
         for i in range(1, int(duration) + 1):
             giorno_key = f'giorno {i}'
             giorno_data = itinerario_citta.get(giorno_key, {})
-            #print("Giorno data:", giorno_data)
-            #for attivita in giorno_data.get("attività", []):
-                #print("Attività:", attivita)
 
             # Ottieni solo le informazioni necessarie per ogni attività del giorno
             attivita_giorno = [{
@@ -48,13 +48,92 @@ def mostra_itinerario(request, name, place, duration):
 
             giorni_itinerario[giorno_key] = attivita_giorno
 
+        # Ottieni le domande vero/falso per la città
+        domande_vero_falso = itinerari_data.get(place.lower(), {}).get('domande_vero_falso', [])
 
-
-
-        #print(giorni_itinerario)
-
-        return render(request, 'myapp/itinerario.html', {'name': name, 'place': place, 'duration': duration, 'giorni_itinerario': giorni_itinerario})
+        return render(request, 'myapp/itinerario.html', {'name': name, 'place': place, 'duration': duration, 'giorni_itinerario': giorni_itinerario, 'domande_vero_falso': domande_vero_falso})
     except FileNotFoundError:
         return HttpResponseNotFound("File JSON non trovato")
     except Exception as e:
         return HttpResponseServerError("Si è verificato un errore durante il recupero dell'itinerario: {}".format(str(e)))
+
+
+def is_risposta_corretta(place, indice, risposta):
+    try:
+        # Leggi il file JSON degli itinerari
+        with open('myapp/itinerari.json') as file:
+            itinerari_data = json.load(file)
+
+        # Trova il luogo nel file JSON
+        itinerario_citta = itinerari_data.get(place.lower(), {})
+        domande_vero_falso = itinerario_citta.get('domande_vero_falso', [])
+
+        # Verifica se la chiave 'domande_vero_falso' è presente nel dizionario
+        if domande_vero_falso:
+            print("Chiave 'domande_vero_falso' trovata")
+            if 0 <= indice < len(domande_vero_falso):
+                print("Indice valido:", indice)
+                # Confronta direttamente le stringhe delle risposte
+                if domande_vero_falso[indice]["risposta"].lower() == risposta['risposta'].lower():
+                    print(f"Domanda {indice} risposta corretta: {risposta['risposta']}")
+                    return True
+                else:
+                    print(f"Domanda {indice} risposta errata: {risposta['risposta']}")
+            else:
+                print("Indice non valido")
+        else:
+            print("Chiave 'domande_vero_falso' non trovata per il luogo specificato")
+
+    except FileNotFoundError:
+        print("File JSON non trovato")
+    except Exception as e:
+        print("Errore durante il caricamento del file JSON:", e)
+
+    return False
+
+
+def calcola_punteggio(place, risposte_utente):
+    punteggio = 0
+    for indice, risposta in risposte_utente.items():
+        if is_risposta_corretta(place, int(indice), risposta):
+            punteggio += 1
+    return punteggio
+
+def rispondi_domanda(request,place):
+    print("Inizio vista rispondi_domanda")
+
+    if request.method == 'POST':
+        risposte_utente = {}
+        try:
+            for key, value in request.POST.items():
+                if key.startswith('risposte'):
+                    index = key.split('[')[1].split(']')[0]
+                    campo = key.split('[')[2].split(']')[0]
+                    if index not in risposte_utente:
+                        risposte_utente[index] = {}
+                    risposte_utente[index][campo] = value
+
+            print("Risposte dell'utente:", risposte_utente)
+
+            punteggio = calcola_punteggio(place, risposte_utente)
+            print("Punteggio:", punteggio)
+
+            print("Tipo di punteggio:", type(punteggio))
+            print("Valore di punteggio:", punteggio)
+            context = {'punteggio': punteggio}
+
+            return render(request, 'myapp/domanda.html', context)
+        except Exception as e:
+            print(f"Errore durante la gestione delle risposte dell'utente: {e}")
+
+    return JsonResponse({'error': 'Metodo non consentito'}, status=405)
+
+def pagina_punteggio(request, punteggio):
+
+      # Ottieni il punteggio dalla query string dell'URL, se presente
+    print("Punteggio attuale:", punteggio)
+
+    return render(request, 'myapp/domanda.html', {'punteggio': punteggio})
+
+
+
