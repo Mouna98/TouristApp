3c1fa745ad627d55695647235940466448965e9e
mou <mouna.erraji@stud.unifi.it>
2024-05-06 17:35:51 +0200

    Aggiunta requirements.txt

--- mytouristsite/mytouristsite/settings.py
+++ mytouristsite/mytouristsite/settings.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,7 +47,7 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Impostazione del SESSION_ENGINE
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
@@ -103,7 +104,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   }
+
+    #"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
